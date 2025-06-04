6a35c6f82ef81fea44528c94af75cf18086d6209
Fussita <hualong.chiang@gmail.com>
2025-06-03 21:40:38 -0400

    del: exp

--- src/_core/utils/result-handler/result.handler.ts
+++ src/_core/utils/result-handler/result.handler.ts
@@ -1,34 +1,33 @@
-import { BadRequestException } from "@nestjs/common"
+import { BadRequestException } from "@nestjs/common";
 
 export class Result<T> {
     private value?: T
     private error?: Error
 
-    private constructor ( value: T, error: Error ) {
-        this.error = error
+    private constructor(value?: T, error?: Error) {
         this.value = value
+        this.error = error
     }
 
-    isSuccess (): boolean {
-        if ( this.value || this.Value == 0) return true
-        return false
+    isSuccess(): boolean {
+        return this.value !== undefined && this.value !== null
     }
 
-    get Value (): T {
-        if ( this.isSuccess ) return this.value
-        throw new BadRequestException( 'The value does not exists' )
+    get Value(): T {
+        if (this.isSuccess()) return this.value!
+        throw new BadRequestException('The value does not exist')
     }
 
-    get Error (): Error {
-        if ( this.error ) return this.error
-        throw new BadRequestException( 'The error does not exists' )
+    get Error(): Error {
+        if (this.error) return this.error
+        throw new BadRequestException('The error does not exist')
     }
 
-    static success<T> ( value: T ): Result<T> {
-        return new Result( value, null )
+    static success<T>(value: T): Result<T> {
+        return new Result<T>(value)
     }
 
-    static fail<T> ( error: Error ): Result<T> {
-        return new Result( null, error )
+    static fail<T>(error: Error): Result<T> {
+        return new Result<T>(undefined, error)
     }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { MongooseDataBaseProvider } from './_core/infraestructure/provider/moongose-db-provider';
+import { ExpController } from './exp/exp.controller';
 
 @Module({
   imports: [
@@ -10,7 +11,9 @@ import { MongooseDataBaseProvider } from './_core/infraestructure/provider/moong
     })
 
   ],
-  controllers: [],
+  controllers: [
+    ExpController
+  ],
   providers: [
     MongooseDataBaseProvider
   ],
